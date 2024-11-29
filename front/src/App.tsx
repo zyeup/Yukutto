67b604e0e0914cbadb08bda22cb2b65713e81466
zyeup <160883122+zyeup@users.noreply.github.com>
2024-11-29 20:26:41 +0900

    ローディング中の表示を追加

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import LoadingSpinner from './components/LoadingSpinner';
 import './App.css'
 import Home from './pages/Home';
 import Map from './pages/map';
@@ -22,6 +23,12 @@ type PostData = {
 const App: React.FC = () => {
 
   const [posts, setPosts] = useState<PostData[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setIsLoading(false), 2000);
+    return () => clearTimeout(timer); // クリーンアップ
+  }, []);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -35,18 +42,24 @@ const App: React.FC = () => {
 
   return (
     <>
-      <Header></Header>
-      <Routes>
-        <Route path="/" element={<Home posts={posts} setPosts={setPosts} />} />
-        <Route path="/map" element={<Map />} />
-        <Route path="/posts" element={<Posts />}>
-          <Route path=":postId" element={<Post posts={posts} />} />
-        </Route>
-        <Route path="/create_posts" element={<CreatePost posts={posts} setPosts={setPosts} />} />
-        <Route path="/edit_posts" element={<EditPosts />}>
-          <Route path=":postId" element={<EditPost posts={posts} setPosts={setPosts} />} />
-        </Route>
-      </Routes>
+    {isLoading ? (
+        <LoadingSpinner />
+      ) : (
+        <div>
+          <Header></Header>
+          <Routes>
+            <Route path="/" element={<Home posts={posts} setPosts={setPosts} />} />
+            <Route path="/map" element={<Map />} />
+            <Route path="/posts" element={<Posts />}>
+              <Route path=":postId" element={<Post posts={posts} />} />
+            </Route>
+            <Route path="/create_posts" element={<CreatePost posts={posts} setPosts={setPosts} />} />
+            <Route path="/edit_posts" element={<EditPosts />}>
+              <Route path=":postId" element={<EditPost posts={posts} setPosts={setPosts} />} />
+            </Route>
+          </Routes>
+       </div>
+     )}
     </>
   );
 };
