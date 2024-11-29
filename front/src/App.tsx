fb36116b2b6dfd80771d12105c830c1da8d5798c
zyeup <160883122+zyeup@users.noreply.github.com>
2024-11-29 21:35:45 +0900

    ローディング画面を改善

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -27,7 +27,7 @@ const App: React.FC = () => {
 
   useEffect(() => {
     const timer = setTimeout(() => setIsLoading(false), 2000);
-    return () => clearTimeout(timer); // クリーンアップ
+    return () => clearTimeout(timer);
   }, []);
 
   useEffect(() => {
@@ -38,6 +38,7 @@ const App: React.FC = () => {
     };
 
     fetchPosts();
+    setIsLoading(false)
   }, []);
 
   return (
