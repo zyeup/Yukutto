2eb0013a8aa47651c6c59e66939b5dd79a8738a0
zyeup <160883122+zyeup@users.noreply.github.com>
2024-11-30 11:33:29 +0900

    tailwindの修正

--- front/src/components/LoadingSpinner.tsx
+++ front/src/components/LoadingSpinner.tsx
@@ -5,7 +5,7 @@ const LoadingSpinner: React.FC = () => {
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <div className="relative w-16 h-16">
         <div className="absolute w-full h-full border-4 border-solid border-gray-300 border-t-blue-500 rounded-full animate-spin"></div>
-        <div className="absolute w-10 h-10 top-3 left-3 border-4 border-solid border-gray-200 border-t-blue-300 rounded-full animate-spin-reverse"></div>
+        <div className="w-12 h-12 border-4 border-solid border-gray-300 border-t-blue-500 rounded-full animate-spin-reverse"></div>
       </div>
     </div>
   );
