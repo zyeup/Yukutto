4e80c5b9b78ab689f2093be44a97ebe0fca359d0
zyeup <160883122+zyeup@users.noreply.github.com>
2024-12-18 12:15:37 +0900

    S3に保存

--- back/config/initializers/carrierwave.rb
+++ back/config/initializers/carrierwave.rb
@@ -1,5 +1,21 @@
+require 'fog/aws'
+
 CarrierWave.configure do |config|
-  config.asset_host = "http://localhost:3000"
-  config.storage = :file
-  config.cache_storage = :file
+  if Rails.env.production?
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region:                ENV['S3_REGION'],
+    }
+    config.fog_directory  = ENV['S3_BUCKET_NAME']
+    config.fog_public     = false
+    config.storage        = :fog
+    config.asset_host     = "https://#{ENV['S3_BUCKET_NAME']}.s3.#{ENV['S3_REGION']}.amazonaws.com"
+  else
+    config.asset_host = "http://localhost:3000"
+    config.storage = :file
+    config.cache_storage = :file
+  end
 end
